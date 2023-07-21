1f9f596a6bfded399daae616ac717b5791e8825f
SONY <SONY@DESKTOP-4A6JHI9>
2023-07-21 09:46:28 +0530

    testcase added 2

--- OrangeHRM/src/testcases/Testcase2.java
+++ OrangeHRM/src/testcases/Testcase2.java
@@ -2,9 +2,9 @@ package testcases;
 
 public class Testcase2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("this is testcase 2");
 	}
 
 }
